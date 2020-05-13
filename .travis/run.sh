f7f973d590c58c951925d573dee50fbc9a7d572d
lukaszlacinski <lukasz@uchicago.edu>
2020-05-12 19:19:09 -0500

    Test

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+date 
+exit 0
+
 cd cime/scripts
 ./create_newcase --case master.A_WCYCL1850.ne4_oQU240.baseline --compset A_WCYCL1850 --res ne4_oQU240
 cd master.A_WCYCL1850.ne4_oQU240.baseline
