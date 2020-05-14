6e3c267c29fc4ceb52599a865c83558bfbc135f5
lukaszlacinski <lukasz@uchicago.edu>
2020-05-14 15:49:37 -0500

    Test

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+go version
+which go
+
 export VERSION=1.13.5 OS=linux ARCH=amd64 && \
     wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
     sudo tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz && \
