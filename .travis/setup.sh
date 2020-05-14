6210a4551a53a3cad4cd68501f350b0f166f1f50
lukaszlacinski <lukasz@uchicago.edu>
2020-05-14 15:58:46 -0500

    Test

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -3,13 +3,13 @@
 go version
 which go
 
-export VERSION=1.13.5 OS=linux ARCH=amd64 && \
-    wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
-    sudo tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz && \
-    rm go$VERSION.$OS-$ARCH.tar.gz
-ls -la /usr/local/go/bin
+#export VERSION=1.13.5 OS=linux ARCH=amd64 && \
+#    wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
+#    sudo tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz && \
+#    rm go$VERSION.$OS-$ARCH.tar.gz
+#ls -la /usr/local/go/bin
 
-export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin
+#export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin
 go version
 export VERSION=3.5.3 && # adjust this as necessary \
     wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz && \
