ee29551c6771e3aa728cf0e9ebba239868dc5a74
lukaszlacinski <lukasz@uchicago.edu>
2020-05-15 13:09:12 -0500

    Test

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
+# Remove the pre-installed Go 1.11 from the environment
 go version
-which go
-export 
-
 for p in $(xargs -n1 -d: <<< $PATH); do
     echo $p | grep '/go' > /dev/null
     if [ $? -ne 0 ]; then
@@ -13,42 +11,24 @@ done
 export PATH="$TMPPATH"
 unset GOPATH
 unset GOROOT
-echo $PATH
-echo $GOROOT
-echo $GOPATH
 
+# Install Go
 export VERSION=1.13.5 OS=linux ARCH=amd64 && \
     wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
     sudo tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz && \
     rm go$VERSION.$OS-$ARCH.tar.gz
-ls -la /usr/local/go/bin
 
 export GOPATH="${HOME}/go"
 export PATH="/usr/local/go/bin:${PATH}:${GOPATH}/bin"
 go version
+
+# Install Singularity
 export VERSION=3.5.3 && # adjust this as necessary \
     wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz && \
     tar -xzf singularity-${VERSION}.tar.gz && \
     cd singularity
-
 ./mconfig && \
     make -C ./builddir && \
     sudo make -C ./builddir install
 
-exit 0
-
-export PATH="${GOPATH}/bin:${PATH}"
-
-mkdir -p "${GOPATH}/src/github.com/sylabs"
-cd "${GOPATH}/src/github.com/sylabs"
-
-git clone https://github.com/sylabs/singularity
-cd singularity
-git checkout tags/v3.4.2
-./mconfig -v -p /usr/local
-make -j `nproc 2>/dev/null || echo 1` -C ./builddir all
-sudo make -C ./builddir install
-
-echo "Singularity version"
 singularity --version
-echo
