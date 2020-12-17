b7822f3e04c714712cc11611508308869c313a2d
lukaszlacinski <lukasz@uchicago.edu>
2020-12-17 12:18:43 -0600

    test

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -2,16 +2,6 @@
 
 go version
 
-# Install Go
-#export VERSION=1.13.5 OS=linux ARCH=amd64 && \
-#    wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
-#    sudo tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz && \
-#    rm go$VERSION.$OS-$ARCH.tar.gz
-
-#export GOPATH="${HOME}/go"
-#export PATH="/usr/local/go/bin:${PATH}:${GOPATH}/bin"
-#go version
-
 # Install Singularity
 export VERSION=3.5.3 && # adjust this as necessary \
     wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz && \
